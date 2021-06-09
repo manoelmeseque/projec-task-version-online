a74247377ddae0fa3f60915975c2c6fdd3108a4c
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-06-09 12:49:47 -0300

    add screens auth

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -9,6 +9,7 @@ import {
 } from 'react-native'
 
 import axios from 'axios'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyle from '../commonStyles'
@@ -66,14 +67,16 @@ export default class Auth extends Component{
                 password: this.state.password, 
             })
 
+            AsyncStorage.setItem('userData', JSON.stringify(res.data))
             axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-            this.props.navigation.navigate('Home')
+            this.props.navigation.navigate('Home', res.data)
 
         }catch(e){
             showError(e)
         }
     }
 
+    
 
 
     render(){
